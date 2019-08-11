ca8983a645586b1687786afec3cc4794a10364a7
adrianwithah <null>
2019-08-11 22:12:39 +0100

    Refactor for PR and add error handling for single-command-execution

--- examples/basic/server/src/lib.rs
+++ examples/basic/server/src/lib.rs
@@ -1,4 +1,3 @@
-extern crate winapi;
 use com::{RawIUnknown, CLASS_E_CLASSNOTAVAILABLE, HRESULT, IID, LPVOID, REFCLSID, REFIID, S_OK};
 use std::convert::TryInto;
 use std::ffi::{CStr, CString};
@@ -35,10 +34,20 @@ use local_file_manager_class::LocalFileManagerClass;
 use windows_file_manager::WindowsFileManager;
 use windows_file_manager_class::WindowsFileManagerClass;
 
-struct registry_key_info {
+struct RegistryKeyInfo {
     key_path: CString,
     key_value_name: CString,
     key_value_data: CString,
+}
+
+impl RegistryKeyInfo {
+    fn new(key_path: String, key_value_name: String, key_value_data: String) -> RegistryKeyInfo {
+        RegistryKeyInfo {
+            key_path: CString::new(key_path).unwrap(),
+            key_value_name: CString::new(key_value_name).unwrap(),
+            key_value_data: CString::new(key_value_data).unwrap(),
+        }
+    }
 }
 
 #[no_mangle]
@@ -81,8 +90,8 @@ extern "stdcall" fn DllRegisterServer() -> HRESULT {
         let registry_keys_to_add = get_relevant_registry_keys();
 
         for key_info in registry_keys_to_add.iter() {
-            let result: u32 = add_class_key(&key_info).try_into().unwrap();
-            if result != ERROR_SUCCESS {
+            let result = add_class_key(&key_info);
+            if result as u32 != ERROR_SUCCESS {
                 println!("Error creating key. error code: {}", result);
                 DllUnregisterServer();
                 return SELFREG_E_CLASS;
@@ -103,8 +112,8 @@ extern "stdcall" fn DllUnregisterServer() -> HRESULT {
         registry_keys_to_remove.reverse();
 
         for key_info in registry_keys_to_remove.iter() {
-            let result: u32 = remove_class_key(&key_info).try_into().unwrap();
-            if result != ERROR_SUCCESS {
+            let result = remove_class_key(&key_info);
+            if result as u32 != ERROR_SUCCESS {
                 println!("Error deleting key. error code: {}", result);
                 hr = SELFREG_E_CLASS;
             }
@@ -114,114 +123,137 @@ extern "stdcall" fn DllUnregisterServer() -> HRESULT {
     }
 }
 
-unsafe fn add_class_key(key_info: &registry_key_info) -> LSTATUS {
+fn create_class_key(key_info: &RegistryKeyInfo) -> Result<HKEY, LSTATUS> {
     let hkResult: HKEY = std::ptr::null_mut::<c_void>() as HKEY;
     let lpClass = std::ptr::null_mut::<CHAR>();
     let lpSecurityAttributes = std::ptr::null_mut::<SECURITY_ATTRIBUTES>();
     let lpdwDisposition = std::ptr::null_mut::<DWORD>();
-    let result = RegCreateKeyExA(
-        HKEY_CLASSES_ROOT,
-        key_info.key_path.as_ptr(),
-        0,
-        lpClass,
-        REG_OPTION_NON_VOLATILE,
-        KEY_ALL_ACCESS,
-        lpSecurityAttributes,
-        &hkResult as *const _ as *mut HKEY,
-        lpdwDisposition,
-    );
+    let result = unsafe {
+        RegCreateKeyExA(
+            HKEY_CLASSES_ROOT,
+            key_info.key_path.as_ptr(),
+            0,
+            lpClass,
+            REG_OPTION_NON_VOLATILE,
+            KEY_ALL_ACCESS,
+            lpSecurityAttributes,
+            &hkResult as *const _ as *mut HKEY,
+            lpdwDisposition,
+        )
+    };
     if result as u32 != ERROR_SUCCESS {
-        println!("Error creating key. error code: {}", result);
-        return result;
+        return Err(result);
     }
 
-    let result = RegSetValueExA(
-        hkResult,
-        key_info.key_value_name.as_ptr(),
-        0,
-        REG_SZ,
-        key_info.key_value_data.as_ptr() as *const u8,
-        key_info
-            .key_value_data
-            .to_bytes_with_nul()
-            .len()
-            .try_into()
-            .unwrap(),
-    );
+    Ok(hkResult)
+}
+
+fn set_class_key(key_handle: HKEY, key_info: &RegistryKeyInfo) -> Result<HKEY, LSTATUS> {
+    let result = unsafe {
+        RegSetValueExA(
+            key_handle,
+            key_info.key_value_name.as_ptr(),
+            0,
+            REG_SZ,
+            key_info.key_value_data.as_ptr() as *const u8,
+            key_info
+                .key_value_data
+                .to_bytes_with_nul()
+                .len()
+                .try_into()
+                .unwrap(),
+        )
+    };
     if result as u32 != ERROR_SUCCESS {
-        println!("Error creating key. error code: {}", result);
-        return result;
+        return Err(result);
     }
 
-    RegCloseKey(hkResult)
+    Ok(key_handle)
 }
 
-unsafe fn remove_class_key(key_info: &registry_key_info) -> LSTATUS {
-    RegDeleteKeyA(HKEY_CLASSES_ROOT, key_info.key_path.as_ptr())
+fn add_class_key(key_info: &RegistryKeyInfo) -> LSTATUS {
+    let key_handle = match create_class_key(key_info) {
+        Ok(key_handle) => key_handle,
+        Err(e) => {
+            println!("Error creating key. error code: {}", e);
+            return e;
+        }
+    };
+
+    let key_handle = match set_class_key(key_handle, key_info) {
+        Ok(key_handle) => key_handle,
+        Err(e) => {
+            println!("Error setting key. error code: {}", e);
+            return e;
+        }
+    };
+
+    unsafe {
+        RegCloseKey(key_handle)
+    }
 }
 
-unsafe fn get_relevant_registry_keys() -> Vec<registry_key_info> {
-    let MAX_FILE_PATH_LENGTH = 260;
-    let hModule = GetModuleHandleA(CString::new("server.dll").unwrap().as_ptr());
-    let raw_ptr = CString::new(Vec::with_capacity(MAX_FILE_PATH_LENGTH))
-        .expect("Failed to create empty string!")
-        .into_raw();
+fn remove_class_key(key_info: &RegistryKeyInfo) -> LSTATUS {
+    unsafe {
+        RegDeleteKeyA(HKEY_CLASSES_ROOT, key_info.key_path.as_ptr())
+    }
+}
 
-    GetModuleFileNameA(hModule, raw_ptr, MAX_FILE_PATH_LENGTH.try_into().unwrap());
+fn get_dll_file_path() -> String {
+    unsafe {
+        let MAX_FILE_PATH_LENGTH = 260;
+        let hModule = GetModuleHandleA(CString::new("server.dll").unwrap().as_ptr());
+        let raw_ptr = CString::new(Vec::with_capacity(MAX_FILE_PATH_LENGTH))
+            .expect("Failed to create empty string!")
+            .into_raw();
 
-    let file_path = CString::from_raw(raw_ptr);
+        GetModuleFileNameA(hModule, raw_ptr, MAX_FILE_PATH_LENGTH.try_into().unwrap());
 
+        CString::from_raw(raw_ptr).into_string().unwrap()
+    }
+}
+
+fn class_key_path(clsid: IID) -> String {
+    format!("CLSID\\{}", clsid.to_string())
+}
+
+fn class_inproc_key_path(clsid: IID) -> String {
+    format!("CLSID\\{}\\InprocServer32", clsid.to_string())
+}
+
+fn get_relevant_registry_keys() -> Vec<RegistryKeyInfo> {
+    let file_path = get_dll_file_path();
     // IMPORTANT: Assumption of order: Subkeys are located at a higher index than the parent key.
     vec![
-        registry_key_info {
-            key_path: CString::new(format!("CLSID\\{}", CLSID_CAT_CLASS.to_string())).unwrap(),
-            key_value_name: CString::new("").unwrap(),
-            key_value_data: CString::new("Cat Component").unwrap(),
-        },
-        registry_key_info {
-            key_path: CString::new(format!(
-                "CLSID\\{}\\InprocServer32",
-                CLSID_CAT_CLASS.to_string()
-            ))
-            .unwrap(),
-            key_value_name: CString::new("").unwrap(),
-            key_value_data: file_path.clone(),
-        },
-        registry_key_info {
-            key_path: CString::new(format!(
-                "CLSID\\{}",
-                CLSID_LOCAL_FILE_MANAGER_CLASS.to_string()
-            ))
-            .unwrap(),
-            key_value_name: CString::new("").unwrap(),
-            key_value_data: CString::new("Local File Manager Component").unwrap(),
-        },
-        registry_key_info {
-            key_path: CString::new(format!(
-                "CLSID\\{}\\InprocServer32",
-                CLSID_LOCAL_FILE_MANAGER_CLASS.to_string()
-            ))
-            .unwrap(),
-            key_value_name: CString::new("").unwrap(),
-            key_value_data: file_path.clone(),
-        },
-        registry_key_info {
-            key_path: CString::new(format!(
-                "CLSID\\{}",
-                CLSID_WINDOWS_FILE_MANAGER_CLASS.to_string()
-            ))
-            .unwrap(),
-            key_value_name: CString::new("").unwrap(),
-            key_value_data: CString::new("Windows File Manager Component").unwrap(),
-        },
-        registry_key_info {
-            key_path: CString::new(format!(
-                "CLSID\\{}\\InprocServer32",
-                CLSID_WINDOWS_FILE_MANAGER_CLASS.to_string()
-            ))
-            .unwrap(),
-            key_value_name: CString::new("").unwrap(),
-            key_value_data: file_path.clone(),
-        },
+        RegistryKeyInfo::new(
+            class_key_path(CLSID_CAT_CLASS),
+            String::from(""),
+            String::from("Cat Component"),
+        ),
+        RegistryKeyInfo::new(
+            class_inproc_key_path(CLSID_CAT_CLASS),
+            String::from(""),
+            file_path.clone(),
+        ),
+        RegistryKeyInfo::new(
+            class_key_path(CLSID_LOCAL_FILE_MANAGER_CLASS),
+            String::from(""),
+            String::from("Local File Manager Component"),
+        ),
+        RegistryKeyInfo::new(
+            class_inproc_key_path(CLSID_LOCAL_FILE_MANAGER_CLASS),
+            String::from(""),
+            file_path.clone(),
+        ),
+        RegistryKeyInfo::new(
+            class_key_path(CLSID_WINDOWS_FILE_MANAGER_CLASS),
+            String::from(""),
+            String::from("Windows File Manager Component"),
+        ),
+        RegistryKeyInfo::new(
+            class_inproc_key_path(CLSID_WINDOWS_FILE_MANAGER_CLASS),
+            String::from(""),
+            file_path.clone(),
+        ),
     ]
 }
