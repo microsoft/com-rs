aca1cbd00f542e1504240f667493be399945eb4a
Ryan Levick <ryan.levick@gmail.com>
2019-09-13 15:41:25 +0200

    Ensure interface methods only take &self

--- examples/aggregation/interface/src/ifile_manager.rs
+++ examples/aggregation/interface/src/ifile_manager.rs
@@ -4,5 +4,5 @@ use winapi::shared::winerror::HRESULT;
 
 #[com_interface(25A41124-23D0-46BE-8351-044889D5E37E)]
 pub trait IFileManager: IUnknown {
-    fn delete_all(&mut self) -> HRESULT;
+    fn delete_all(&self) -> HRESULT;
 }
