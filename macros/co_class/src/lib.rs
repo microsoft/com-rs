8c588b491655b7e7b9ecf970f1fa90ad4041ce64
Ryan Levick <ryan.levick@gmail.com>
2019-09-17 17:38:53 +0200

    Decide whether to do aggregatable

--- macros/co_class/src/lib.rs
+++ macros/co_class/src/lib.rs
@@ -10,25 +10,18 @@ mod com_struct_impl;
 mod drop_impl;
 pub mod iunknown_impl;
 
-// Macro expansion entry point.
-pub fn expand_co_class(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(item as ItemStruct);
-    let attr_args = syn::parse_macro_input!(attr as AttributeArgs);
+pub fn expand_co_class(input: &ItemStruct, attr_args: &AttributeArgs) -> TokenStream {
+    let base_interface_idents = macro_utils::base_interface_idents(attr_args);
+    let aggr_interface_idents = macro_utils::get_aggr_map(attr_args);
 
-    // Parse attributes
-    let base_interface_idents = macro_utils::base_interface_idents(&attr_args);
-    let aggr_interface_idents = macro_utils::get_aggr_map(&attr_args);
-
-    let mut out: Vec<TokenStream> = Vec::new();
-    out.push(com_struct::generate(&aggr_interface_idents, &base_interface_idents, &input).into());
+    let mut out = Vec::<TokenStream>::new();
+    out.push(com_struct::generate(&aggr_interface_idents, &base_interface_idents, input).into());
     out.push(
-        com_struct_impl::generate(&aggr_interface_idents, &base_interface_idents, &input).into(),
+        com_struct_impl::generate(&aggr_interface_idents, &base_interface_idents, input).into(),
     );
-    out.push(
-        iunknown_impl::generate(&base_interface_idents, &aggr_interface_idents, &input).into(),
-    );
-    out.push(drop_impl::generate(&aggr_interface_idents, &base_interface_idents, &input).into());
-    out.push(class_factory::generate(&input).into());
+    out.push(iunknown_impl::generate(&base_interface_idents, &aggr_interface_idents, input).into());
+    out.push(drop_impl::generate(&aggr_interface_idents, &base_interface_idents, input).into());
+    out.push(class_factory::generate(input).into());
 
     TokenStream::from_iter(out)
 }
