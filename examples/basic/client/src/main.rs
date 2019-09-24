7772d448c8df9387db09043a0a6d1bea13caa157
Ryan Levick <ryan.levick@gmail.com>
2019-09-24 13:36:49 +0200

    Reduce amount of code in example client

--- examples/basic/client/src/main.rs
+++ examples/basic/client/src/main.rs
@@ -2,98 +2,51 @@ use com::{interfaces::iunknown::IUnknown, runtime::ApartmentThreadedRuntime as R
 use interface::{IAnimal, ICat, IDomesticAnimal, IExample, CLSID_CAT_CLASS};
 
 fn main() {
-    let runtime = match Runtime::new() {
-        Ok(runtime) => runtime,
-        Err(hr) => {
-            println!("Failed to initialize COM Library: {}", hr);
-            return;
-        }
-    };
+    let runtime =
+        Runtime::new().unwrap_or_else(|hr| panic!("Failed to initialize COM Library{:x}", hr));
 
-    let factory = match runtime.get_class_object(&CLSID_CAT_CLASS) {
-        Ok(factory) => {
-            println!("Got cat class object");
-            factory
-        }
-        Err(hr) => {
-            println!("Failed to get cat class object {:x}", hr as u32);
-            return;
-        }
-    };
+    let factory = runtime
+        .get_class_object(&CLSID_CAT_CLASS)
+        .unwrap_or_else(|hr| panic!("Failed to get cat class object {:x}", hr));
+    println!("Got cat class object");
 
-    let unknown = match factory.get_instance::<dyn IUnknown>() {
-        Some(unknown) => {
-            println!("Got IUnknown");
-            unknown
-        }
-        None => {
-            println!("Failed to get IUnknown");
-            return;
-        }
-    };
+    let unknown = factory
+        .get_instance::<dyn IUnknown>()
+        .expect("Failed to get IUknown");
+    println!("Got IUnknown");
 
-    let animal = match unknown.get_interface::<dyn IAnimal>() {
-        Some(animal) => {
-            println!("Got IAnimal");
-            animal
-        }
-        None => {
-            println!("Failed to get an IAnimal");
-            return;
-        }
-    };
+    let animal = unknown
+        .get_interface::<dyn IAnimal>()
+        .expect("Failed to get IAnimal");
+    println!("Got IAnimal");
 
     animal.eat();
 
     // Test cross-vtable interface queries for both directions.
-    let domestic_animal = match animal.get_interface::<dyn IDomesticAnimal>() {
-        Some(domestic_animal) => {
-            println!("Got IDomesticAnimal");
-            domestic_animal
-        }
-        None => {
-            println!("Failed to get IDomesticAnimal");
-            return;
-        }
-    };
+    let domestic_animal = animal
+        .get_interface::<dyn IDomesticAnimal>()
+        .expect("Failed to get IDomesticAnimal");
+    println!("Got IDomesticAnimal");
 
     domestic_animal.train();
 
-    let new_cat = match domestic_animal.get_interface::<dyn ICat>() {
-        Some(new_cat) => {
-            println!("Got ICat");
-            new_cat
-        }
-        None => {
-            println!("Failed to get ICat");
-            return;
-        }
-    };
+    let new_cat = domestic_animal
+        .get_interface::<dyn ICat>()
+        .expect("Failed to get ICat");
+    println!("Got ICat");
     new_cat.ignore_humans();
 
     // Test querying within second vtable.
-    let domestic_animal_two = match domestic_animal.get_interface::<dyn IDomesticAnimal>() {
-        Some(domestic_animal_two) => {
-            println!("Got IDomesticAnimal");
-            domestic_animal_two
-        }
-        None => {
-            println!("Failed to get domestic animal!");
-            return;
-        }
-    };
+    let domestic_animal_two = domestic_animal
+        .get_interface::<dyn IDomesticAnimal>()
+        .expect("Failed to get second IDomesticAnimal");
+    println!("Got IDomesticAnimal");
     domestic_animal_two.train();
 
-    let cat = match runtime.create_instance::<dyn ICat>(&CLSID_CAT_CLASS) {
-        Ok(cat) => {
-            println!("Got another cat");
-            cat
-        }
-        Err(e) => {
-            println!("Failed to get an cat, {:x}", e);
-            return;
-        }
-    };
+    let cat = runtime
+        .create_instance::<dyn ICat>(&CLSID_CAT_CLASS)
+        .unwrap_or_else(|hr| panic!("Failed to get a cat {:x}", hr));
+    println!("Got another cat");
 
     cat.eat();
 
